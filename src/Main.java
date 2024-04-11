49fa1c2b78432cd88b5f556717908cdafd103b17
eehanseul <gkstmf616@gmail.com>
2024-04-11 18:23:08 +0900

    feat: db connect

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Connection con = null;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String server = "";
+        String database = "";
+        String user_name = "";
+        String password = "";
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println(" 드라이버 로딩 오류 : " + e.getMessage());
+            e.printStackTrace();
         }
+
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://" +
+                    server + "/" +
+                    database +
+                    "?useSSL=false", user_name, password); // SSL 실행 확인
+            System.out.println("연결 성공");
+
+            Statement stmt = con.createStatement();
+            ResultSet resultSet = stmt.executeQuery("SELECT * FROM User;");
+            resultSet.next();
+
+            System.out.println(resultSet.getString(2));
+
+        } catch(SQLException e) {
+            System.err.println("에러 내용 :" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            if(con != null)
+                con.close();
+        } catch (SQLException e) {}
     }
 }
