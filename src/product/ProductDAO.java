14d2d70a8c6ca7ab4714490298503643fa5a6cd0
NOEL-code <junusong12@hanyang.ac.kr>
2024-04-11 17:43:14 +0900

    프로젝트 파일 생성

--- src/product/ProductDAO.java
+++ src/product/ProductDAO.java
@@ -1,4 +1,5 @@
 package product;
 
-public interface ProductDAO {
+public interface ProductDAO   {
+
 }
--- src/sale/SalesService.java
+++ src/sale/SalesService.java
@@ -1,4 +1,4 @@
 package sale;
 
-public interface SalesService {
+public class SalesService {
 }
