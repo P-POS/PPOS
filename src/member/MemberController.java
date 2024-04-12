d81b2277a2e11e5a63805375611366f1b374c330
ye-s-rin <pyr53540@gmail.com>
2024-04-12 15:07:28 +0900

    feat: MemberController

--- src/member/MemberController.java
+++ src/member/MemberController.java
@@ -1,4 +1,12 @@
 package member;
 
 public class MemberController {
+
+    MemberModel memberModel;
+    MemberView memberView;
+
+    MemberController(MemberModel memberModel) {
+        this.memberModel = memberModel;
+        this.memberView = new MemberView(this);
+    }
 }
