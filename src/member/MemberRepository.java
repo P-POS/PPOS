97a4a14c55a41e0499259d77c85933481faf5c76
ye-s-rin <pyr53540@gmail.com>
2024-04-12 15:58:05 +0900

    feat: MemberRepository

---
+++ src/member/MemberRepository.java
@@ -0,0 +1,39 @@
+package member;
+
+public class MemberRepository implements MemberDAO{
+
+    @Override
+    public void createMember(String memberName) {
+
+    }
+
+    @Override
+    public void deleteMember(int memberId) {
+
+    }
+
+    @Override
+    public void updateMember(int memberId, String memberName, int memberPoint) {
+
+    }
+
+    @Override
+    public String getMemberName(int memberId) {
+        return null;
+    }
+
+    @Override
+    public void setMemberName(int memberId, String memberName) {
+
+    }
+
+    @Override
+    public int getMemberPoint(int memberId) {
+        return 0;
+    }
+
+    @Override
+    public void setMemberPoint(int memberId, int memberPoint) {
+
+    }
+}
