b102e5fafd65631021b23b37c15b335afbafa09e
ye-s-rin <pyr53540@gmail.com>
2024-04-12 15:23:31 +0900

    feat: MemberPopupController

--- src/memberPopup/MemberPopupController.java
+++ src/memberPopup/MemberPopupController.java
@@ -1,5 +1,14 @@
 package memberPopup;
 
+import member.MemberView;
+
 public class MemberPopupController {
 
+    MemberPopupModel memberPopupModel;
+    MemberPopupView memberPopupView;
+
+    MemberPopupController(MemberPopupModel memberPopupModel) {
+        this.memberPopupModel = memberPopupModel;
+        this.memberPopupView = new MemberPopupView(this);
+    }
 }
