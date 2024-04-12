d71c3f158c6282d8fdd812c6243d51d0bb1dfab9
ye-s-rin <pyr53540@gmail.com>
2024-04-12 15:54:59 +0900

    feat: MemberDAO

--- src/member/MemberDAO.java
+++ src/member/MemberDAO.java
@@ -1,4 +1,18 @@
 package member;
 
 public interface MemberDAO {
+
+    void createMember(String memberName);
+
+    void deleteMember(int memberId);
+
+    void updateMember(int memberId, String memberName, int memberPoint);
+
+    String getMemberName(int memberId);
+
+    void setMemberName(int memberId, String memberName);
+
+    int getMemberPoint(int memberId);
+
+    void setMemberPoint(int memberId, int memberPoint);
 }
