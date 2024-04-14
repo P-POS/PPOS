58fbca824ba7e4d5fa91a71e58f68675d5394564
yerin <pyr53540@gmail.com>
2024-04-15 00:11:48 +0900

    feat: MemberModel

--- src/member/MemberModel.java
+++ src/member/MemberModel.java
@@ -1,9 +1,37 @@
 package member;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class MemberModel {
+    private int memberId;
+    private String memberName;
+    private int memberScore;
 
+    public MemberModel(int memberId, String memberName, int memberScore) {
+        this.memberId = memberId;
+        this.memberName = memberName;
+        this.memberScore = memberScore;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public int getMemberScore() {
+        return memberScore;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public void setMemberScore(int memberScore) {
+        this.memberScore = memberScore;
+    }
 }
