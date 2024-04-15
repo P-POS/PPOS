cc17315ebda80fb6a59b8e725787eda7d724ea99
kim hyun soo <hyjkl0124@naver.com>
2024-04-16 08:37:35 +0900

    no message

--- src/MartAPP.java
+++ src/MartAPP.java
@@ -10,21 +10,6 @@ import sale.SalesService;
 public class MartAPP {
     public static void main(String[] args) {
 //      new main.MainView(new MainController());
-        SaleController saleController = new SaleController(new SalesService());
-        System.out.println(saleController.getMemberInfo(1).getClientName());
-        saleController.getProductInfo(1);
-        saleController.getProductInfo(2);
-        ArrayList<ProductOrderNumDTO> productOrderNumDTOS = saleController.getProductInfo(3);
-       saleController.cancleProducts();
-
-        System.out.println("-------");
-        System.out.println(saleController.getProductInfo(2));
-        saleController.getProductInfo(3);
-        saleController.getProductInfo(4);
-        System.out.println(saleController.returnProducts(2));
-        for(ProductOrderNumDTO productOrderNumDTO : productOrderNumDTOS){
-            System.out.println(productOrderNumDTO.getProductDTO().getProductPrice());
-        }
-
+        new SaleController(new SalesService());
     }
 }
--- src/sale/ProductRepository.java
+++ src/sale/ProductRepository.java
@@ -55,6 +55,4 @@ public class ProductRepository implements ProductDAO {
         }
 
     }
-
-
 }
