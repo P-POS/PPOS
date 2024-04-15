b0fe2b904b314765b2dbc0263fea4ba944936d28
ye-s-rin <pyr53540@gmail.com>
2024-04-16 08:47:05 +0900

    refactor: void -> boolean

--- src/member/MemberController.java
+++ src/member/MemberController.java
@@ -8,11 +8,13 @@ public class MemberController {
     private MemberView memberView;
 
     public MemberController(MemberService memberService) {
+
         this.memberService = memberService;
         this.memberView = new MemberView(this);
     }
 
     public boolean createMember(int memberId, String memberName, Integer memberScore) {
+
         if (memberScore == null) {
             memberScore = 0;
         }
@@ -25,24 +27,39 @@ public class MemberController {
         return true;
     }
 
-    public void deleteMember(int memberId) {
-        memberService.deleteMember(memberId);
+    public boolean deleteMember(int memberId) {
+
+        try {
+            memberService.deleteMember(memberId);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
     }
 
-    public void updateMember(int memberId, String memberName, int memberScore) {
-        MemberModel memberModel = new MemberModel(memberId, memberName, memberScore);
-        memberService.updateMember(memberModel);
+    public boolean updateMember(int memberId, String memberName, int memberScore) {
+
+        try {
+            MemberModel memberModel = new MemberModel(memberId, memberName, memberScore);
+            memberService.updateMember(memberModel);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
     }
 
     public ArrayList getMember(int memberId) {
+
         return memberService.getMember(memberId);
     }
 
     public ArrayList getMemberUseName(String memberName) {
+
         return memberService.getMemberUseName(memberName);
     }
 
     public ArrayList getAllMembers() {
+
         ArrayList<MemberModel> members = memberService.getAllMembers();
         if (!members.isEmpty()) {
             return members;
@@ -52,6 +69,7 @@ public class MemberController {
     }
 
     String getLatestSaleDate(int memberId) {
+
         String latestSaleDate = memberService.getLatestSaleDate(memberId);
         if (latestSaleDate != null) {
             return latestSaleDate;
