7a3a8511897253bc8cf68000bc2491d4ef90ea09
Tomk2d <uijinshin01@gmail.com>
2024-04-15 10:39:04 +0900

    임시 뷰

--- src/MartAPP.java
+++ src/MartAPP.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 
-public class Main {
+public class MartAPP {
 
     public static void main(String[] args) {
         Connection con = null;
