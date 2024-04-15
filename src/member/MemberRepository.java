82a1ac0e3c8421fe9d430aea8bf7a88c63628bbe
ye-s-rin <pyr53540@gmail.com>
2024-04-15 15:04:24 +0900

    fix

--- src/member/MemberRepository.java
+++ src/member/MemberRepository.java
@@ -4,7 +4,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
 import DBConnection.DBConnection;
 
 public class MemberRepository implements MemberDAO {
