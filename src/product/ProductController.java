1ea5eab2736ab6e7e6a36e9051eda198c5386cb3
yjp8842 <yjp20183029@gmail.com>
2024-04-16 10:27:29 +0900

    feat: 컨트롤러, 뷰 구조 변경

--- src/product/ProductController.java
+++ src/product/ProductController.java
@@ -3,15 +3,21 @@ package product;
 import main.MainController;
 
 import java.util.ArrayList;
+import main.MainController;
 
 public class ProductController {
-
     private MainController mainController;
-    ProductService productService = new ProductService();
 
-//    public ProductController(MainController mainController){
-//        this.mainController = mainController;
-//    }
+    public ProductController(MainController mainController) {
+        this.mainController = mainController;
+        new ProductView(this);
+    }
+
+    public void openMainPage(){
+        mainController.openMainPage();
+    }
+
+    ProductService productService = new ProductService();
 
     public Product getProduct(int productNum) {
        return new Product(productService.getProduct(productNum));
@@ -30,15 +36,14 @@ public class ProductController {
     }
 
     public boolean addProduct(Product product) {
-        return productService.addProduct(new ProductReqDTO(product));
+        return productService.addProduct(new ProductReqDTO());
     }
 
     public boolean updateProduct(Product product) {
-        return productService.updateProduct(new ProductReqDTO(product));
+        return productService.updateProduct(new ProductReqDTO());
     }
 
     public boolean deleteProduct(int productNum) {
         return productService.deleteProduct(productNum);
     }
-
 }
